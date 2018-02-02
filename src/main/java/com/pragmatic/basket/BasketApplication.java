6a1c4f78c9c40798e66521ee6ee225ef57e86b6d
Rafał Janus <RJanus@wip.pl>
2018-02-02 11:11:32 +0100

    Virtual basket 0.9

--- src/main/java/com/pragmatic/basket/BasketApplication.java
+++ src/main/java/com/pragmatic/basket/BasketApplication.java
@@ -1,12 +1,7 @@
 package com.pragmatic.basket;
 
-import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
-
-import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
 public class BasketApplication {
@@ -14,5 +9,4 @@ public class BasketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BasketApplication.class, args);
 	}
-
 }
